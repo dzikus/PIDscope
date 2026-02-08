9aadc92629c9ba6414840c69940629f1b36a7472
Grzegorz Sterniczuk <grzegorz@sternicz.uk>
2026-02-08 20:48:05 +0100

    test: add Octave test framework with 24 tests for core algorithms and compat shims

---
+++ tests/test_PTthrSpec.m
@@ -0,0 +1,36 @@
+% test_PTthrSpec.m - Tests for throttle x frequency spectrogram
+% NOTE: PTthrSpec expects X,Y as column vectors, F in kHz
+
+%!test
+%! % Smoke test: function runs on synthetic data
+%! Fs_khz = 4;
+%! Fs = Fs_khz * 1000;
+%! duration = 5;
+%! N = Fs * duration;
+%! throttle = ones(N, 1) * 50;
+%! signal = sin(2 * pi * 150 * (0:N-1)' / Fs);
+%! [freq, ampMat] = PTthrSpec(throttle, signal, Fs_khz, 0);
+%! assert(~isempty(ampMat));
+%! assert(~isempty(freq));
+
+%!test
+%! % Output matrix is 100 rows (throttle bins)
+%! Fs_khz = 4;
+%! Fs = Fs_khz * 1000;
+%! N = Fs * 5;
+%! throttle = ones(N, 1) * 50;
+%! signal = randn(N, 1);
+%! [~, ampMat] = PTthrSpec(throttle, signal, Fs_khz, 0);
+%! assert(size(ampMat, 1), 100);
+
+%!test
+%! % Energy concentrated at correct throttle bin
+%! Fs_khz = 4;
+%! Fs = Fs_khz * 1000;
+%! N = Fs * 10;
+%! throttle = ones(N, 1) * 50;
+%! signal = sin(2 * pi * 100 * (0:N-1)' / Fs);
+%! [~, ampMat] = PTthrSpec(throttle, signal, Fs_khz, 0);
+%! energy_at_50 = sum(ampMat(50, :));
+%! energy_at_10 = sum(ampMat(10, :));
+%! assert(energy_at_50 > energy_at_10);
